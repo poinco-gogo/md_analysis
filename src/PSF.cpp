41307f4cc31e00ec47c1be8ebe722b1d82792ed1
Koichi Tamura <koichi.tamura@riken.jp>
2019-08-12 21:50:10 +0900

    adapted to treat dihed/improp/cmap

--- src/PSF.cpp
+++ src/PSF.cpp
@@ -62,6 +62,30 @@ void PSF::read_PSF()
 			is >> ncnt;
 			get_angle_list(ncnt, fi);
 		}
+
+		loc = s.find("NPHI", 0);
+		if (loc != string::npos)
+		{
+			istringstream is(s.substr(0, 8));
+			is >> ncnt;
+			get_dihedral_list(ncnt, fi);
+		}
+
+		loc = s.find("NIMPHI", 0);
+		if (loc != string::npos)
+		{
+			istringstream is(s.substr(0, 8));
+			is >> ncnt;
+			get_improper_list(ncnt, fi);
+		}
+
+		loc = s.find("NCRTERM", 0);
+		if (loc != string::npos)
+		{
+			istringstream is(s.substr(0, 8));
+			is >> ncnt;
+			get_cmap_list(ncnt, fi);
+		}
 	}
 
 	fi.close();
@@ -289,6 +313,372 @@ bool PSF::set_angle_parm(vector<Angle>& angleParmVector)
 
 } // end of func() set_angle_parm
 
+void PSF::get_dihedral_list(const int ndihedral, ifstream& fi)
+{
+	cout
+	<< "REMARK NUMBER OF UNIQUE DIHEDRAL FROM PSF : " << ndihedral << '\n';
+
+	string s;
+	int b;
+	while ( getline(fi, s) && s != "" )
+	{
+		istringstream is(s);
+		while (is >> b)
+			dihedralArray.push_back(b);
+	}
+
+	/*  Debug      list all dihedral  */
+/*
+	for (int i = 0; i < ndihedral; i++)
+	{
+		cout << "REMARK  "
+		     << "dihedral no. " << i+1 << " / " << ndihedral << " "
+		     << dihedralArray[i*4    ] << " - "
+		     << dihedralArray[i*4 + 1] << " - "
+		     << dihedralArray[i*4 + 2] << " - "
+		     << dihedralArray[i*4 + 3] << " "
+		     << ptr_atomVector -> at(dihedralArray[i*4] - 1).PSFAtomName
+		     << " - "
+		     << ptr_atomVector -> at(dihedralArray[i*4 + 1] - 1).PSFAtomName
+		     << " - "
+		     << ptr_atomVector -> at(dihedralArray[i*4 + 2] - 1).PSFAtomName
+		     << " - "
+		     << ptr_atomVector -> at(dihedralArray[i*4 + 3] - 1).PSFAtomName << '\n';
+	}*/
+
+}
+
+bool PSF::set_dihedral_parm(vector<Dihedral>& dihedralParmVector)
+{
+	string iat1, iat2, iat3, iat4;
+	string jat1, jat2, jat3, jat4;
+
+	// now make dihedralVector
+	for (int i = 0; i < dihedralArray.size() / 4; i++)
+	{
+		bool assigned = false;
+
+		int i4 = i * 4;
+
+		int id1 = dihedralArray[i4    ] - 1;
+		int id2 = dihedralArray[i4 + 1] - 1;
+		int id3 = dihedralArray[i4 + 2] - 1;
+		int id4 = dihedralArray[i4 + 3] - 1;
+
+		iat1 = ptr_atomVector -> at( id1 ).PSFAtomName;
+		iat2 = ptr_atomVector -> at( id2 ).PSFAtomName;
+		iat3 = ptr_atomVector -> at( id3 ).PSFAtomName;
+		iat4 = ptr_atomVector -> at( id4 ).PSFAtomName;
+
+		for (Dihedral& dparm: dihedralParmVector)
+		{
+			// skip wildcard parameters
+			if (dparm.is_wildcard) continue;
+
+			jat1 = dparm.at1; jat2 = dparm.at2;
+			jat3 = dparm.at3; jat4 = dparm.at4;
+
+			if ((iat1 == jat1 && iat2 == jat2 && iat3 == jat3 && iat4 == jat4) || (iat1 == jat4 && iat2 == jat3 && iat3 == jat2 && iat4 == jat1))
+			{
+				assigned = true;
+
+				Dihedral dihed(
+				&ptr_atomVector -> at( id1 ),
+				&ptr_atomVector -> at( id2 ),
+				&ptr_atomVector -> at( id3 ),
+				&ptr_atomVector -> at( id4 ),
+				dparm.Kchi,
+				dparm.n,
+				dparm.delta);
+
+				dihedralVector.push_back(dihed);
+
+				// ここでbreakすると
+			        // O    C    N    CP1 みたいにパラメータを
+				// 複数持つ二面角にパラメータがアサインされない.
+				//break;
+			}
+		}
+
+		// forward to next dihedral if assigned
+		if (assigned) continue;
+
+		// paramaeres for this dihedral is missing.
+		// search for wildcard parameters
+		for (Dihedral& dparm: dihedralParmVector)
+		{
+			// skip non-wildcard parameters
+			if (!dparm.is_wildcard) continue;
+
+			jat1 = dparm.at1; jat2 = dparm.at2;
+			jat3 = dparm.at3; jat4 = dparm.at4;
+
+			// assumed X-A-B-X, not A-X-X-B... is this OK?
+			if ( (iat2 == jat2 && iat3 == jat3) ||
+					(iat2 == jat3 && iat3 == jat2))
+			{
+				assigned = true;
+
+				Dihedral dihed(
+				&ptr_atomVector -> at( id1 ),
+				&ptr_atomVector -> at( id2 ),
+				&ptr_atomVector -> at( id3 ),
+				&ptr_atomVector -> at( id4 ),
+				dparm.Kchi,
+				dparm.n,
+				dparm.delta);
+
+				dihedralVector.push_back(dihed);
+			}
+		}
+
+		if (!assigned)
+		{
+			cerr << "error: Dihedral parameter missing for: "
+					<< iat1 << " - " << iat2 << " - "
+					<< iat3 << " - " << iat4 << '\n';
+			return false;
+		}
+	} // for dihedralArray
+/*
+	// DEBUG: list all dihedral
+	for (int i = 0; i < DihedralVector.size(); i++)
+	{
+		cout << "DEBUG " << i + 1 << " / " << DihedralVector.size()
+		<< " "
+		<< DihedralVector[i].ptr_atom1 -> PSFAtomName << " - "
+		<< DihedralVector[i].ptr_atom2 -> PSFAtomName << " - "
+		<< DihedralVector[i].ptr_atom3 -> PSFAtomName << " - "
+		<< DihedralVector[i].ptr_atom4 -> PSFAtomName << "   "
+		<< DihedralVector[i].Kchi << "  "
+		<< DihedralVector[i].n    << "  "
+		<< DihedralVector[i].delta << endl;
+	}
+
+	cout << "DEBUG dihedral " << dihedralArray.size() / 4 << " should be "
+	<< DihedralVector.size() << '\n';
+*/
+	cout << "REMARK Dihedral parameters successfully assigned.\n";
+	cout << "REMARK NUMBER OF DUPLICATED DIHEDRAL = "
+		<< dihedralVector.size() << '\n';
+
+	dihedralArray.clear();
+	dihedralParmVector.clear();
+
+	return true;
+
+} // end of func() set_dihedral_parm
+
+
+void PSF::get_improper_list(int nimproper, ifstream& fi)
+{
+	cout << "REMARK NUMBER OF IMPROPER FROM PSF : " << nimproper << '\n';
+
+	string s;
+	int b;
+	while ( getline(fi, s) && s != "" )
+	{
+		istringstream is(s);
+		while (is >> b)
+			improperArray.push_back(b);
+	}
+/*
+	for (int i = 0; i < nimproper; i++)
+	{
+		cout << "REMARK  "
+		     << "improper no. " << i+1 << " / " << nimproper << " "
+		     << improperArray[i*4    ] << " - "
+		     << improperArray[i*4 + 1] << " - "
+		     << improperArray[i*4 + 2] << " - "
+		     << improperArray[i*4 + 3] << " "
+		     << ptr_atomVector -> at(improperArray[i*4] - 1).PDBAtomName
+		     << " - "
+		     << ptr_atomVector -> at(improperArray[i*4 + 1] - 1).PDBAtomName
+		     << " - "
+		     << ptr_atomVector -> at(improperArray[i*4 + 2] - 1).PDBAtomName
+		     << " - "
+		     << ptr_atomVector -> at(improperArray[i*4 + 3] - 1).PDBAtomName << '\n';
+	}
+*/
+} // end of func() get_improper_list
+
+bool PSF::set_improper_parm(vector<Improper>& improperParmVector)
+{
+	for (int i = 0; i < improperArray.size() / 4; i++)
+	{
+		bool assigned = false;
+
+		int id1 = improperArray[i * 4    ];
+		int id2 = improperArray[i * 4 + 1];
+		int id3 = improperArray[i * 4 + 2];
+		int id4 = improperArray[i * 4 + 3];
+
+		string iat1 = ptr_atomVector -> at(id1 - 1).PSFAtomName;
+		string iat2 = ptr_atomVector -> at(id2 - 1).PSFAtomName;
+		string iat3 = ptr_atomVector -> at(id3 - 1).PSFAtomName;
+		string iat4 = ptr_atomVector -> at(id4 - 1).PSFAtomName;
+
+		for (Improper& iparm: improperParmVector)
+		{
+			// skip wildcard parameters
+			if (iparm.is_wildcard) continue;
+
+			string jat1 = iparm.at1;
+			string jat2 = iparm.at2;
+			string jat3 = iparm.at3;
+			string jat4 = iparm.at4;
+
+			if ((iat1 == jat1 && iat2 == jat2 && iat3 == jat3 && iat4 == jat4) || (iat1 == jat4 && iat2 == jat3 && iat3 == jat2 && iat4 == jat1))
+			{
+				assigned = true;
+
+				Improper impro(
+				&ptr_atomVector -> at(id1-1),
+				&ptr_atomVector -> at(id2-1),
+				&ptr_atomVector -> at(id3-1),
+				&ptr_atomVector -> at(id4-1),
+				iparm.Kpsi,
+				iparm.psi0);
+
+				improperVector.push_back(impro);
+
+				break;
+			}
+		}
+
+		if (assigned) continue;
+
+		// then try wildcard parameters
+		for (Improper& iparm: improperParmVector)
+		{
+			// skip non-wildcard parameters
+			if (!iparm.is_wildcard) continue;
+
+			string jat1 = iparm.at1;
+			string jat2 = iparm.at2;
+			string jat3 = iparm.at3;
+			string jat4 = iparm.at4;
+
+			if ((iat1 == jat1 && iat4 == jat4) || (iat1 == jat4 && iat4 == jat1))
+			{
+				assigned = true;
+
+				Improper impro(
+				&ptr_atomVector -> at(id1-1),
+				&ptr_atomVector -> at(id2-1),
+				&ptr_atomVector -> at(id3-1),
+				&ptr_atomVector -> at(id4-1),
+				iparm.Kpsi,
+				iparm.psi0);
+
+				improperVector.push_back(impro);
+
+				break;
+			}
+		}
+
+		if (!assigned)
+		{
+			cerr << "error: improper parameter missing for: "
+				<< iat1 << " - " << iat2 << " - "
+				<< iat3 << " - " << iat4 << '\n';
+			return false;
+		}
+	}
+
+	improperArray.clear();
+	improperParmVector.clear();
+	cout << "REMARK Improper parameters successfully assigned.\n";
+	return true;
+/*
+	cout << "DEBUG IMPROPER ARRAY.size() / 4 = " << improperArray.size() / 4
+	<< " should be \"assigned\" " << assigned << '\n';
+	for (int i = 0; i < ImproperVector.size(); i++)
+	{
+		Improper* impro = &ImproperVector[i];
+		cout << i + 1 << "   "
+		<< impro->ptr_atom1->PSFIndex << " "
+		<< impro->ptr_atom1->PSFAtomName << " "
+		<< impro->ptr_atom2->PSFIndex << " "
+		<< impro->ptr_atom2->PSFAtomName << " "
+		<< impro->ptr_atom3->PSFIndex << " "
+		<< impro->ptr_atom3->PSFAtomName << " "
+		<< impro->ptr_atom4->PSFIndex << " "
+		<< impro->ptr_atom4->PSFAtomName << " "
+		<< impro->Kpsi << " "
+		<< impro->psi0 << endl;
+	}
+*/
+}// end of func() set_improper_parm
+
+void PSF::get_cmap_list(int ncmap, ifstream& fi)
+{
+	cout << "REMARK NUMBER OF CROSS-TERM : "  << ncmap << '\n';
+	string s;
+	while(getline(fi, s) && !s.empty())
+	{
+		istringstream is(s);
+		int itmp;
+		while (is >> itmp) cmapArray.push_back(itmp);
+	}
+/*	cout << "cmapArray.size() / 4 = " << cmapArray.size() / 4 << endl;
+	cout << "DEBUG>>>>>>>>>>>>>>>>>>>>>>>\n";
+	for (int i = 0; i < ncrossterm * 8; i = i + 4)
+	{
+		//showAtom(&ptr_atomVector->at(cmapArray[i] - 1));
+		int i1 = cmapArray[i    ]; int i2 = cmapArray[i + 1];
+		int i3 = cmapArray[i + 2]; int i4 = cmapArray[i + 3];
+		cout << i1 << " " << i2 << " " << i3 << " " << i4 << '\n';
+	}*/
+}
+
+void PSF::set_cmap_parm(vector<Cmap>& cmapParmVector)
+{
+	for (int i = 0; i < cmapArray.size() / 8; i++)
+	{
+		vector<Atom*>  p_at(8);
+		vector<string> attype(8);
+		for (int j = 0; j < 8; j++)
+		{
+			p_at[j] =
+			&ptr_atomVector->at(cmapArray[8 * i + j] - 1);
+
+			attype[j] = p_at[j]->PSFAtomName;
+		}
+
+		Dihedral dphi(p_at[0], p_at[1], p_at[2], p_at[3], 0., 0., 0.);
+		Dihedral dpsi(p_at[4], p_at[5], p_at[6], p_at[7], 0., 0., 0.);
+
+		Cmap cmap(dphi, dpsi);
+
+		for (int j = 0; j < cmapParmVector.size(); j++)
+		{
+			bool match = true;
+			for (int k = 0; k < 8; k++)
+			{
+				if (cmapParmVector[j].attype[k]
+					!= attype[k])
+				{
+					match = false;
+					break;
+				}
+			}
+			if (match)
+			{
+				cmap.attype = cmapParmVector[j].attype;
+
+				cmap.cmap_type_index = j;
+
+				break;
+			}
+		}
+
+		cmapVector.push_back(cmap);
+	}
+
+	cout << "REMARK " << cmapVector.size() << " cmap created.\n";
+}
+
 bool PSF::set_lj_parm(vector<Atom>& LJParmVector)
 {
 	if (!LJParmVector.size())
@@ -362,6 +752,18 @@ void PSF::make_exclusion_vector()
 	}
 
 } // end of function PSF::make_exclusion_vector()
+
+void PSF::make_scaled1_4_vector()
+{
+	for (auto& dihed: dihedralVector)
+	{
+		Atom* atom1 = dihed.ptr_atom1;
+		Atom* atom4 = dihed.ptr_atom4;
+
+		atom1 -> scaled1_4Vector.push_back(atom4 -> PSFIndex);
+		atom4 -> scaled1_4Vector.push_back(atom1 -> PSFIndex);
+	}
+}
 
 void PSF::writePDB(string filename, string header, const vector<int>& indexVector)
 {
--- src/PSF.hpp
+++ src/PSF.hpp
@@ -8,6 +8,9 @@
 #include "Atom.hpp"
 #include "Bond.hpp"
 #include "Angle.hpp"
+#include "Dihedral.hpp"
+#include "Improper.hpp"
+#include "Cmap.hpp"
 
 class PSF 
 {
@@ -20,9 +23,15 @@ class PSF
 	std::vector<Atom>      atomVector;
 	std::vector<Bond>      bondVector;
 	std::vector<Angle>     angleVector;
+	std::vector<Dihedral>  dihedralVector;
+	std::vector<Improper>  improperVector;
+	std::vector<Cmap>      cmapVector;
 
 	std::vector<int> bondArray;
 	std::vector<int> angleArray;
+	std::vector<int> dihedralArray;
+	std::vector<int> improperArray;
+	std::vector<int> cmapArray;
 	
 	// constructor
 	PSF(std::string filename);
@@ -33,9 +42,13 @@ class PSF
 
 	bool set_bond_parm(std::vector<Bond>& bondParmVector);
 	bool set_angle_parm(std::vector<Angle>& angleParmVector);
+	bool set_dihedral_parm(std::vector<Dihedral>& dihedralParmVector);
+	bool set_improper_parm(std::vector<Improper>& improperParmVector);
+	void set_cmap_parm(std::vector<Cmap>& cmapParmVector);
 	bool set_lj_parm(std::vector<Atom>& LJParmVector);
 
 	void make_exclusion_vector();
+	void make_scaled1_4_vector();
 
 	void showAtom(const Atom& atom);
 	void showResidue(int resID);
@@ -56,5 +69,8 @@ class PSF
 	void get_atom_list(const int natom, std::ifstream& fi);
 	void get_bond_list(const int nbond, std::ifstream& fi);
 	void get_angle_list(const int nangle, std::ifstream& fi);
+	void get_dihedral_list(const int ndihedral, std::ifstream& fi);
+	void get_improper_list(const int nimproper, std::ifstream& fi);
+	void get_cmap_list(const int ncmap, std::ifstream& fi);
 };
 #endif
