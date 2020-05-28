614aeb0a571f2bf56f8067739fe09d47db59bd1e
Koichi Tamura <koichi.tamura@riken.jp>
2020-05-28 18:15:54 +0900

    add new files

---
+++ src/d_trj_extract.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+#include "PSF.hpp"
+#include "ReadDCD.hpp"
+#include "Index.hpp"
+using namespace std;
+int main(int argc, char** argv)
+{
+	if (argc < 4)
+	{
+		cout << 
+		"\nusage: ./a.out psf dcd ind\n\n";
+		return 1;
+	}
+
+	vector<Atom> atomVector;
+
+	PSF PSFFile(argv[1], &atomVector);
+
+	ReadDCD DCD(&atomVector);
+	if (!DCD.open_dcd_read(argv[2]))
+		return 0;
+
+	Index idx(argv[3]);
+	vector<int> cvs;
+	if (!idx.load_indexes(&cvs))
+		return 0;
+	cout << "REMARK " << cvs.size() << " indexes loaded.\n";
+
+	cout << setprecision(8) << scientific;
+	while (DCD.read_1step())
+	{
+		cout << setw(16) << DCD._nsteps();
+
+		for (auto& i: cvs)
+		{
+			Atom& atom = PSFFile.atomVector[i - 1];
+
+			cout
+				<< setw(16) << atom.position.x()
+				<< setw(16) << atom.position.y()
+				<< setw(16) << atom.position.z();
+		}
+		cout << '\n';
+	}
+}
