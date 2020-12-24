8e5f4aea14b5cdb1ea93a667c74bd476e9f3e845
Koichi Tamura <koichi.tamura@riken.jp>
2020-12-24 12:49:16 +0900

    enable 2d kde plot with MBAR weight

--- src/ComputeKDE.cpp
+++ src/ComputeKDE.cpp
@@ -19,6 +19,24 @@ ComputeKDE::ComputeKDE(vector<double>* ptr_dataVector1,	vector<double>* ptr_data
 	this->band_width         = band_width;
 }
 
+ComputeKDE::ComputeKDE(vector<double>* ptr_dataVector1,	vector<double>* ptr_dataVector2, vector<double>* ptr_weightVector, double min1, double max1, int nbin1, double band_width1, double min2, double max2, int nbin2, double band_width2)
+{
+	this->ptr_dataVector1    = ptr_dataVector1;
+	this->ptr_dataVector2    = ptr_dataVector2;
+
+	this->ptr_weightVector   = ptr_weightVector;
+
+	this->min1               = min1;
+	this->max1               = max1;
+	this->nbin1              = nbin1;
+	this->band_width1        = band_width1;
+
+	this->min2               = min2;
+	this->max2               = max2;
+	this->nbin2              = nbin2;
+	this->band_width2        = band_width2;
+}
+
 double ComputeKDE::estimate_gauss(double x)
 {
 	double sum = 0;
@@ -66,6 +84,26 @@ double ComputeKDE::estimate_gauss_weight(double x)
 		sum += ptr_dataVector2->at(icnt++) * exp( - xx * 0.5 );
 	}
 	sum /= sqrt( 2.0 * N_PI ) * ptr_dataVector1->size() * band_width;
+
+	return sum;
+}
+
+double ComputeKDE::estimate_gauss_weight(double x, double y)
+{
+	double sum = 0;
+	for (int i = 0; i < ptr_dataVector1->size(); i++)
+	{
+		double xdata = ptr_dataVector1->at(i);
+		double ydata = ptr_dataVector2->at(i);
+
+		double xx = ( x - xdata ) / band_width1;
+		double yy = ( y - ydata ) / band_width2;
+		xx *= xx;
+		yy *= yy;
+
+		sum += ptr_weightVector->at(i) * exp( - ( xx + yy ) * 0.5 );
+	}
+	sum /= 2.0 * N_PI * ptr_dataVector1->size() * band_width1 * band_width2;
 
 	return sum;
 }
--- src/ComputeKDE.hpp
+++ src/ComputeKDE.hpp
@@ -8,7 +8,11 @@ class ComputeKDE
 	private:
 	std::vector<double>* ptr_dataVector1;
 	std::vector<double>* ptr_dataVector2;
+	std::vector<double>* ptr_weightVector;
 	double band_width;
+	double min1, max1, band_width1;
+	double min2, max2, band_width2;
+	int nbin1, nbin2;
 
 	public:
 	ComputeKDE(std::vector<double>* ptr_dataVector1, double band_width);
@@ -17,9 +21,17 @@ class ComputeKDE
 			std::vector<double>* ptr_dataVector2,
 			double band_width
 		);
+	ComputeKDE(
+			std::vector<double>* ptr_dataVector1,
+			std::vector<double>* ptr_dataVector2,
+			std::vector<double>* ptr_weightVector,
+			double min1, double max1, int nbin1, double band_width1,
+			double min2, double max2, int nbin2, double band_width2
+		);
 
 	double estimate_gauss(double x);
 	double estimate_gauss(double x, double y);
 	double estimate_gauss_weight(double x);
+	double estimate_gauss_weight(double x, double y);
 };
 #endif
